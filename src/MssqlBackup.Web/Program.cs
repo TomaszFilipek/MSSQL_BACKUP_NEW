646600268d5536edad7643b6ee141b687fe01ef9
Tomasz Filipek <tomasz.filipek@3qcode.com>
2026-08-25 18:22:10 +0200

    Fix Blazor za Cloudflare: ForwardedHeaders + usun HSTS

--- src/MssqlBackup.Web/Program.cs
+++ src/MssqlBackup.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using MssqlBackup.Web.Hubs;
 using MssqlBackup.Web.Services;
 
@@ -11,14 +12,22 @@ builder.Services.AddHttpClient<BackupApiService>(client =>
     client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"] ?? "http://localhost:8283");
 });
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
 
 app.UseStaticFiles();
